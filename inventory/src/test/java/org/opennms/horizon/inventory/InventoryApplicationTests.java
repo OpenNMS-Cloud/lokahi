9558942a7f9c0e01f7daf694329a9aa5b0d87aa5
Thomas Bigger <thomasbigger584@gmail.com>
2022-10-18 14:10:14 +0100

    HS-465,HS-466,HS-467,HS-468: Create Inventory Service

--- inventory/src/test/java/org/opennms/horizon/inventory/InventoryApplicationTests.java
+++ inventory/src/test/java/org/opennms/horizon/inventory/InventoryApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 

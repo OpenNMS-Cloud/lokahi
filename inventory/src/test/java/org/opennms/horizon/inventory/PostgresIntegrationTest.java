482838024d52c96677b11620cdbf9c25237097f5
Thomas Bigger <thomasbigger584@gmail.com>
2022-10-19 13:52:58 +0100

    HS-465,HS-466,HS-467,HS-468: Create Inventory Service

--- inventory/src/test/java/org/opennms/horizon/inventory/PostgresIntegrationTest.java
+++ inventory/src/test/java/org/opennms/horizon/inventory/PostgresIntegrationTest.java
@@ -1,6 +1,8 @@
 package org.opennms.horizon.inventory;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -8,6 +10,15 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -19,6 +30,9 @@ class PostgresIntegrationTest {
         .withDatabaseName("inventory").withUsername("inventory")
         .withPassword("password").withExposedPorts(5432);
 
+    @Autowired
+    private DataSource dataSource;
+
     @DynamicPropertySource
     static void registerDatasourceProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url",
@@ -27,9 +41,31 @@ class PostgresIntegrationTest {
         registry.add("spring.datasource.password", () -> postgres.getPassword());
     }
 
-    @Test
-    void testPostgresContainer() {
+    @BeforeEach
+    public void setup() {
         assertTrue(postgres.isCreated());
         assertTrue(postgres.isRunning());
+    }
+
+    @Test
+    void testPostgresLiquibaseMigration() throws Exception {
+
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("select * from databasechangelog")) {
+
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+            int columnsNumber = resultSetMetaData.getColumnCount();
+
+            List<String> changelogs = new ArrayList<>();
+
+            while (resultSet.next()) {
+                for (int index = 1; index <= columnsNumber; index++) {
+                    changelogs.add(resultSet.getString("id"));
+                    assertEquals("EXECUTED", resultSet.getString("exectype"));
+                }
+            }
+            assertTrue(changelogs.size() > 0);
+        }
     }
 }

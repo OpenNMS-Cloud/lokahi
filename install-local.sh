544c442abf3dcc74711c98ff6ffba4ab5f90ded8
jaberry <jaberry@opennms.com>
2022-10-20 15:19:48 -0400

    HS-483 - testing external-it cluster build

--- install-local.sh
+++ install-local.sh
@@ -5,13 +5,52 @@ cd operator/
 
 bash scripts/create-kind-cluster.sh
 
-echo
-echo ________________Installing Operator________________
-echo
-helm upgrade -i operator-local ../charts/opennms-operator -f ../install-local-operator-values.yaml --namespace opennms --create-namespace
-if [ $? -ne 0 ]; then exit; fi
+# This determines whether or not to import custom images or not.
+HELP="Need to pass 'local' parameter to script"
+if [ -z "$1" ]; then
+  echo "1 - $HELP"
+else
+  if [ $1 == "local" ]; then
+    # Will add a kind-registry here at some point, see .github/ for sample script.
+    kind load docker-image opennms/operator:local-build&
+    kind load docker-image opennms/horizon-stream-core:local&
+    kind load docker-image opennms/horizon-stream-minion:local&
+    kind load docker-image opennms/horizon-stream-minion-gateway:local&
+    kind load docker-image opennms/horizon-stream-keycloak:local&
+    kind load docker-image opennms/horizon-stream-grafana:local&
+    kind load docker-image opennms/horizon-stream-ui:local&
+    kind load docker-image opennms/horizon-stream-notification:local&
+    kind load docker-image opennms/horizon-stream-rest-server:local&
 
-bash scripts/create-instance.sh
+    # Need to wait for the images to be loaded.
+    sleep 120
+
+    echo
+    echo ________________Installing Operator________________
+    echo
+    helm upgrade -i operator-local ../charts/opennms-operator -f scripts/local-operator-values.yaml --namespace opennms --create-namespace
+    if [ $? -ne 0 ]; then exit; fi
+
+    bash scripts/create-instance.sh
+  
+  elif [ $1 == "custom-images" ]; then
+
+    echo
+    echo ________________Installing Operator________________
+    echo
+    helm upgrade -i operator-local ../charts/opennms-operator -f ../install-local-operator-values.yaml --namespace opennms --create-namespace
+    if [ $? -ne 0 ]; then exit; fi
+
+    echo
+    echo ____________Installing Local Instance______________
+    echo
+    kubectl apply -f install-local-onms-instance.yaml
+    if [ $? -ne 0 ]; then exit; f
+
+  else
+    echo "2 - $HELP"
+  fi
+fi
 
 bash scripts/add-local-ssl-cert.sh
 

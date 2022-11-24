4fb9ccda615ac98fac3b7cedacf8e0ed587606cd
Thomas Bigger <thomasbigger584@gmail.com>
2022-11-24 15:19:26 +0000

    HS-603: Add Event gRPC Service to query events

--- events/persistence/src/main/java/org/opennms/horizon/events/persistence/service/EventService.java
+++ events/persistence/src/main/java/org/opennms/horizon/events/persistence/service/EventService.java
@@ -3,7 +3,6 @@ package org.opennms.horizon.events.persistence.service;
 import org.opennms.horizon.events.persistence.mapper.EventMapper;
 import org.opennms.horizon.events.persistence.repository.EventRepository;
 import org.opennms.horizon.events.proto.EventDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,12 +10,13 @@ import java.util.stream.Collectors;
 
 @Component
 public class EventService {
+    private final EventRepository eventRepository;
+    private final EventMapper eventMapper;
 
-    @Autowired
-    private EventRepository eventRepository;
-
-    @Autowired
-    private EventMapper eventMapper;
+    public EventService(EventRepository eventRepository, EventMapper eventMapper) {
+        this.eventRepository = eventRepository;
+        this.eventMapper = eventMapper;
+    }
 
     public List<EventDTO> findEvents() {
         return eventRepository.findAll().stream()

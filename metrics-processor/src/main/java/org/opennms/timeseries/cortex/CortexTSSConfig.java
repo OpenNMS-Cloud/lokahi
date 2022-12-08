646b76ba83bc40e8ae196f25b8bf7e2da89b4430
Benjamin Janssens <bjanssens@opennms.com>
2022-12-08 10:48:31 -0500

    Add cortex
    HS-597 - BTO: Metrics processor should push metrics to Cortex

---
+++ metrics-processor/src/main/java/org/opennms/timeseries/cortex/CortexTSSConfig.java
@@ -0,0 +1,180 @@
+package org.opennms.timeseries.cortex;
+
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class CortexTSSConfig {
+    private final String writeUrl;
+    private final String readUrl;
+    private final int maxConcurrentHttpConnections;
+    private final long writeTimeoutInMs;
+    private final long readTimeoutInMs;
+    private final long metricCacheSize;
+    private final long externalTagsCacheSize;
+    private final long bulkheadMaxWaitDurationInMs;
+    private final String organizationId;
+    private final boolean hasOrganizationId;
+
+    public CortexTSSConfig() {
+        this(builder());
+    }
+
+    public CortexTSSConfig(Builder builder) {
+        this.writeUrl = Objects.requireNonNull(builder.writeUrl);
+        this.readUrl = Objects.requireNonNull(builder.readUrl);
+        this.maxConcurrentHttpConnections = builder.maxConcurrentHttpConnections;
+        this.writeTimeoutInMs = builder.writeTimeoutInMs;
+        this.readTimeoutInMs = builder.readTimeoutInMs;
+        this.metricCacheSize = builder.metricCacheSize;
+        this.externalTagsCacheSize = builder.externalTagsCacheSize;
+        this.bulkheadMaxWaitDurationInMs = builder.bulkheadMaxWaitDurationInMs;
+        this.organizationId = builder.organizationId;
+        this.hasOrganizationId = organizationId != null && organizationId.trim().length() > 0;
+    }
+
+    /**
+     * Will be called via blueprint. The builder can be called when not running as Osgi plugin.
+     */
+    @SuppressWarnings("java:S107")
+    public CortexTSSConfig(
+        final String writeUrl,
+        final String readUrl,
+        final int maxConcurrentHttpConnections,
+        final long writeTimeoutInMs,
+        final long readTimeoutInMs,
+        final long metricCacheSize,
+        final long externalTagsCacheSize,
+        final long bulkheadMaxWaitDurationInMs,
+        final String organizationId) {
+        this(builder()
+            .writeUrl(writeUrl)
+            .readUrl(readUrl)
+            .maxConcurrentHttpConnections(maxConcurrentHttpConnections)
+            .writeTimeoutInMs(writeTimeoutInMs)
+            .readTimeoutInMs(readTimeoutInMs)
+            .metricCacheSize(metricCacheSize)
+            .externalCacheSize(externalTagsCacheSize)
+            .bulkheadMaxWaitDurationInMs(bulkheadMaxWaitDurationInMs)
+            .organizationId(organizationId));
+    }
+
+    public String getWriteUrl() {
+        return writeUrl;
+    }
+
+    public String getReadUrl() {
+        return readUrl;
+    }
+
+    public int getMaxConcurrentHttpConnections() {
+        return maxConcurrentHttpConnections;
+    }
+
+    public long getWriteTimeoutInMs() {
+        return writeTimeoutInMs;
+    }
+
+    public long getReadTimeoutInMs() {
+        return readTimeoutInMs;
+    }
+
+    public long getMetricCacheSize() {
+        return metricCacheSize;
+    }
+
+    public long getExternalTagsCacheSize() {
+        return externalTagsCacheSize;
+    }
+
+    public long getBulkheadMaxWaitDurationInMs() {
+        return bulkheadMaxWaitDurationInMs;
+    }
+
+    public boolean hasOrganizationId() {
+        return hasOrganizationId;
+    }
+
+    public String getOrganizationId() {
+        return organizationId;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private String writeUrl = "http://localhost:9009/api/prom/push";
+        private String readUrl = "http://localhost:9009/prometheus/api/v1";
+        private int maxConcurrentHttpConnections = 100;
+        private long writeTimeoutInMs = 1000;
+        private long readTimeoutInMs = 1000;
+        private long metricCacheSize = 1000;
+        private long externalTagsCacheSize = 1000;
+        private long bulkheadMaxWaitDurationInMs = Long.MAX_VALUE;
+        private String organizationId = null;
+
+        public Builder writeUrl(final String writeUrl) {
+            this.writeUrl = writeUrl;
+            return this;
+        }
+
+        public Builder readUrl(final String readUrl) {
+            this.readUrl = readUrl;
+            return this;
+        }
+
+        public Builder maxConcurrentHttpConnections(final int maxConcurrentHttpConnections) {
+            this.maxConcurrentHttpConnections = maxConcurrentHttpConnections;
+            return this;
+        }
+
+        public Builder writeTimeoutInMs(final long writeTimeoutInMs) {
+            this.writeTimeoutInMs = writeTimeoutInMs;
+            return this;
+        }
+
+        public Builder readTimeoutInMs(final long readTimeoutInMs) {
+            this.readTimeoutInMs = readTimeoutInMs;
+            return this;
+        }
+
+        public Builder metricCacheSize(final long metricCacheSize) {
+            this.metricCacheSize = metricCacheSize;
+            return this;
+        }
+
+        public Builder externalCacheSize(final long externalTagsCacheSize) {
+            this.externalTagsCacheSize = externalTagsCacheSize;
+            return this;
+        }
+
+        public Builder bulkheadMaxWaitDurationInMs(final long bulkheadMaxWaitDurationInMs) {
+            this.bulkheadMaxWaitDurationInMs = bulkheadMaxWaitDurationInMs;
+            return this;
+        }
+
+        public Builder organizationId(final String organizationId) {
+            this.organizationId = organizationId;
+            return this;
+        }
+
+        public CortexTSSConfig build() {
+            return new CortexTSSConfig(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", CortexTSSConfig.class.getSimpleName() + "[", "]")
+            .add("writeUrl='" + writeUrl + "'")
+            .add("readUrl='" + readUrl + "'")
+            .add("maxConcurrentHttpConnections=" + maxConcurrentHttpConnections)
+            .add("writeTimeoutInMs=" + writeTimeoutInMs)
+            .add("readTimeoutInMs=" + readTimeoutInMs)
+            .add("metricCacheSize=" + metricCacheSize)
+            .add("externalCacheSize=" + externalTagsCacheSize)
+            .add("bulkheadMaxWaitDurationInMs=" + bulkheadMaxWaitDurationInMs)
+            .add("organizationId=" + organizationId)
+            .toString();
+    }
+}
---
+++ metrics-processor/src/test/java/org/opennms/timeseries/cortex/ResultMapperTest.java
@@ -0,0 +1,94 @@
+package org.opennms.timeseries.cortex;
+
+import static org.junit.Assert.assertEquals;
+import static org.opennms.timeseries.cortex.CortexTSS.CORTEX_TSS;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.json.JSONObject;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opennms.integration.api.v1.timeseries.IntrinsicTagNames;
+import org.opennms.integration.api.v1.timeseries.Metric;
+import org.opennms.integration.api.v1.timeseries.Sample;
+import org.opennms.integration.api.v1.timeseries.immutables.ImmutableMetric;
+
+public class ResultMapperTest {
+
+    private Metric expectedMetric;
+    private KVStoreMock kvstore;
+
+    @Before
+    public void setUp() {
+        expectedMetric = ImmutableMetric.builder()
+            .intrinsicTag(IntrinsicTagNames.name, "na8793e6f6477407bbd105bf6ed36b698")
+            .intrinsicTag(IntrinsicTagNames.resourceId, "snmp:1:opennms-jvm:org_opennms_newts_name_ring_buffer_max_size_unit=unknown")
+            .metaTag("_idx0", "(snmp,4)")
+            .metaTag("_idx1", "(snmp:1,4)")
+            .metaTag("_idx2", "(snmp:1:opennms-jvm,4)")
+            .metaTag("_idx2w", "(snmp:1,*)")
+            .metaTag("_idx3", "(snmp:1:opennms-jvm:OpenNMS_Name_Notifd,4)")
+            .externalTag("key", "value")
+            .metaTag("host", "myHost1")
+            .metaTag("mtype", "counter")
+            .build();
+        kvstore = new KVStoreMock();
+    }
+
+    @Test
+    public void shouldMapSeriesQueryResult() throws IOException, URISyntaxException {
+        String json = readStringFromFile("seriesQueryResult.json");
+        kvstore.put(expectedMetric.getKey(), new JSONObject().put("key", "value"), CORTEX_TSS);
+        List<Metric> metrics = ResultMapper.fromSeriesQueryResult(json, kvstore);
+        assertEquals(1, metrics.size());
+        assertEquals(expectedMetric, metrics.get(0));
+    }
+
+    @Test
+    public void shouldMapRangeQueryResult() throws IOException, URISyntaxException {
+        String json = readStringFromFile("rangeQueryResult.json");
+        List<Sample> samples = ResultMapper.fromRangeQueryResult(json, expectedMetric);
+
+        assertEquals(expectedMetric, samples.get(0).getMetric());
+        assertEquals(Instant.ofEpochSecond(1602783564), samples.get(0).getTime());
+        assertEquals((Double) 42.3, samples.get(0).getValue());
+        assertEquals(60, samples.size());
+    }
+
+    @Test
+    public void testAppendExternalTagsToMetric() throws IOException, URISyntaxException {
+        String json = readStringFromFile("seriesQueryResult.json");
+        kvstore.put(expectedMetric.getKey(),
+            new JSONObject()
+                .put("key1", "value1")
+                .put("key2", "value2")
+                .put("key3", "value3"),
+            CORTEX_TSS);
+        List<Metric> metrics = ResultMapper.fromSeriesQueryResult(json, kvstore);
+        assertEquals(1, metrics.size());
+        assertEquals(3, metrics.get(0).getExternalTags().size());
+        assertEquals("value3", metrics.get(0).getExternalTags()
+            .stream()
+            .filter(tag -> tag.getKey().equals("key3"))
+            .findFirst()
+            .get()
+            .getValue());
+    }
+
+    private String readStringFromFile(final String fileName) throws IOException, URISyntaxException {
+        StringBuilder contentBuilder = new StringBuilder();
+        try (Stream<String> stream = Files.lines(
+            Paths.get(this.getClass().getResource(fileName).toURI()), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s).append("\n"));
+        }
+        return contentBuilder.toString();
+    }
+}

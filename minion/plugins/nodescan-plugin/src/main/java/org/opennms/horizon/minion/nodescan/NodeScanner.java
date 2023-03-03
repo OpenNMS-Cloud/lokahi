c624a1b9603e694f993ca1f850c90b71dc0a897b
Benjamin Janssens <bjanssens@opennms.com>
2023-03-03 13:07:33 +0100

    Minion:  SnmpAgentConfigDiscovery
    Add SnmpConfigDiscovery.java which is responsible for detecting the SNMP service.
    The detectSNMP method returns a list of SnmpAgentConfig objects that have the SNMP service running
    
    HS-1106

--- minion/plugins/nodescan-plugin/src/main/java/org/opennms/horizon/minion/nodescan/NodeScanner.java
+++ minion/plugins/nodescan-plugin/src/main/java/org/opennms/horizon/minion/nodescan/NodeScanner.java
@@ -30,6 +30,7 @@ package org.opennms.horizon.minion.nodescan;
 
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
@@ -53,9 +54,11 @@ import com.google.protobuf.Any;
 public class NodeScanner implements Scanner {
     private static final Logger LOGGER = LoggerFactory.getLogger(NodeScanner.class);
     private final SnmpHelper snmpHelper;
+    private final SnmpConfigDiscovery snmpConfigDiscovery;
 
     public NodeScanner(SnmpHelper snmpHelper) {
         this.snmpHelper = snmpHelper;
+        snmpConfigDiscovery = new SnmpConfigDiscovery(snmpHelper);
     }
 
     @Override
@@ -72,10 +75,14 @@ public class NodeScanner implements Scanner {
                 SnmpAgentConfig agentConfig = new SnmpAgentConfig(InetAddress.getByName(scanRequest.getPrimaryIp()), SnmpConfiguration.DEFAULTS);
                 agentConfig.setVersion(SnmpConfiguration.VERSION2C);
                 NodeInfoResult nodeInfo = scanSystem(agentConfig);
+                List<SnmpAgentConfig> snmpAgentConfigs = snmpConfigDiscovery.detectSNMP(Collections.singletonList(agentConfig));
 
+                if(snmpAgentConfigs.size() != 1 ) {
+                    throw new RuntimeException("Expected to find one SNMP agent config, but found " + snmpAgentConfigs.size());
+                }
 
-                List<IpInterfaceResult> ipInterfaceResults = scanIpAddrTable(agentConfig);
-                List<SnmpInterfaceResult> snmpInterfaceResults = scanSnmpInterface(agentConfig);
+                List<IpInterfaceResult> ipInterfaceResults = scanIpAddrTable(snmpAgentConfigs.get(0));
+                List<SnmpInterfaceResult> snmpInterfaceResults = scanSnmpInterface(snmpAgentConfigs.get(0));
                 NodeScanResult scanResult = NodeScanResult.newBuilder()
                     .setNodeId(scanRequest.getNodeId())
                     .setNodeInfo(nodeInfo)
---
+++ minion/plugins/nodescan-plugin/src/test/java/org/opennms/horizon/minion/nodescan/SnmpConfigDiscoveryTest.java
@@ -0,0 +1,60 @@
+package org.opennms.horizon.minion.nodescan;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.opennms.horizon.shared.snmp.SnmpAgentConfig;
+import org.opennms.horizon.shared.snmp.SnmpConfiguration;
+import org.opennms.horizon.shared.snmp.SnmpHelper;
+import org.opennms.horizon.shared.snmp.SnmpObjId;
+import org.opennms.horizon.shared.snmp.SnmpValue;
+import org.opennms.horizon.shared.snmp.snmp4j.Snmp4JValue;
+import org.opennms.horizon.shared.utils.InetAddressUtils;
+import org.snmp4j.smi.Integer32;
+
+class SnmpConfigDiscoveryTest {
+    @Mock
+    private SnmpHelper snmpHelper;
+
+    private SnmpConfigDiscovery snmpConfigDiscovery;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        snmpConfigDiscovery = new SnmpConfigDiscovery(snmpHelper);
+    }
+
+    @Test
+    void detectSNMP() {
+        SnmpAgentConfig config1 = new SnmpAgentConfig(InetAddressUtils.getInetAddress("127.0.0.1"), SnmpConfiguration.DEFAULTS);
+        SnmpAgentConfig config2 = new SnmpAgentConfig(InetAddressUtils.getInetAddress("192.168.1.1"), SnmpConfiguration.DEFAULTS);
+        List<SnmpAgentConfig> configs = Arrays.asList(config1, config2);
+
+        SnmpValue[] snmpValues1 = new SnmpValue[]{new Snmp4JValue(new Integer32(1))};
+        SnmpValue[] snmpValues2 = new SnmpValue[0];
+
+        when(snmpHelper.getAsync(config1, new SnmpObjId[]{SnmpObjId.get(SnmpHelper.SYSTEM_OID)}))
+            .thenReturn(CompletableFuture.completedFuture(snmpValues1));
+        when(snmpHelper.getAsync(config2, new SnmpObjId[]{SnmpObjId.get(SnmpHelper.SYSTEM_OID)}))
+            .thenReturn(CompletableFuture.completedFuture(snmpValues2));
+
+        List<SnmpAgentConfig> detectedConfigs = snmpConfigDiscovery.detectSNMP(configs);
+
+        assertEquals(1, detectedConfigs.size());
+        assertEquals(config1, detectedConfigs.get(0));
+
+        verify(snmpHelper, times(2)).getAsync(any(), any());
+    }
+}
+

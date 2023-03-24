d546145fcace3a8ec7e3bd06efbcc9fb11d9d2f7
Rob Ellis <rellis@opennms.com>
2023-03-24 13:47:44 +0000

    HS-1261: Switch to using liquibase changelog-master for includes

--- alert/src/main/java/org/opennms/horizon/alertservice/db/entity/AlertDefinition.java
+++ alert/src/main/java/org/opennms/horizon/alertservice/db/entity/AlertDefinition.java
@@ -35,7 +35,6 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;

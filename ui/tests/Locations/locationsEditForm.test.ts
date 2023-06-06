c3cb3bfd30e8ccf2faffd31b3f5e2128a206967a
mikewrosey <MRose@nanthealth.com>
2023-06-06 16:58:06 -0400

    fix formatting

--- ui/tests/Locations/locationsEditForm.test.ts
+++ ui/tests/Locations/locationsEditForm.test.ts
@@ -17,37 +17,37 @@ describe('LocationsEditForm', () => {
   test('Mount', () => {
     expect(wrapper).toBeTruthy()
   })
-  
+
   test('Should have a headline', () => {
     const elem = wrapper.get('[data-test="headline"]')
     expect(elem.exists()).toBeTruthy()
   })
-  
+
   test('Should have an input name', () => {
     const elem = wrapper.get('[data-test="input-name"]')
     expect(elem.exists()).toBeTruthy()
   })
-  
+
   test('Should have an input longitude', () => {
     const elem = wrapper.get('[data-test="input-longitude"]')
     expect(elem.exists()).toBeTruthy()
   })
-  
+
   test('Should have an input latitude', () => {
     const elem = wrapper.get('[data-test="input-latitude"]')
     expect(elem.exists()).toBeTruthy()
   })
-  
+
   test('Should have a regenrate key button', () => {
     const elem = wrapper.get('[data-test="revoke-btn"]')
     expect(elem.exists()).toBeTruthy()
   })
-  
+
   test('Should have a download button', () => {
     const elem = wrapper.get('[data-test="download-btn"]')
     expect(elem.exists()).toBeTruthy()
   })
-  
+
   test('Should have a save button', () => {
     const elem = wrapper.get('[data-test="save-button"]')
     expect(elem.exists()).toBeTruthy()

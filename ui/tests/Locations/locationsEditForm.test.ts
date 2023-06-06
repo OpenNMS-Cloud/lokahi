79bb63de2006ead03e3897cafe6da184019d65f1
mikewrosey <MRose@nanthealth.com>
2023-06-06 16:55:25 -0400

    wrap loc edit tests

--- ui/tests/Locations/locationsEditForm.test.ts
+++ ui/tests/Locations/locationsEditForm.test.ts
@@ -1,51 +1,55 @@
 import mount from '../mountWithPiniaVillus'
 import LocationsEditForm from '@/components/Locations/LocationsEditForm.vue'
 
-vi.mock('@/components/utils')
+let wrapper: any
 
-const wrapper = mount({
-  component: LocationsEditForm,
-  shallow: false,
-  props: {
-    id: 1
-  }
-})
+describe('LocationsEditForm', () => {
+  beforeAll(() => {
+    wrapper = mount({
+      component: LocationsEditForm,
+      shallow: false
+    })
+  })
+  afterAll(() => {
+    wrapper.unmount()
+  })
 
-test('Mount', () => {
-  expect(wrapper).toBeTruthy()
-})
-
-test('Should have a headline', () => {
-  const elem = wrapper.get('[data-test="headline"]')
-  expect(elem.exists()).toBeTruthy()
-})
-
-test('Should have an input name', () => {
-  const elem = wrapper.get('[data-test="input-name"]')
-  expect(elem.exists()).toBeTruthy()
-})
-
-test('Should have an input longitude', () => {
-  const elem = wrapper.get('[data-test="input-longitude"]')
-  expect(elem.exists()).toBeTruthy()
-})
-
-test('Should have an input latitude', () => {
-  const elem = wrapper.get('[data-test="input-latitude"]')
-  expect(elem.exists()).toBeTruthy()
-})
-
-test('Should have a regenrate key button', () => {
-  const elem = wrapper.get('[data-test="revoke-btn"]')
-  expect(elem.exists()).toBeTruthy()
-})
-
-test('Should have a download button', () => {
-  const elem = wrapper.get('[data-test="download-btn"]')
-  expect(elem.exists()).toBeTruthy()
-})
-
-test('Should have a save button', () => {
-  const elem = wrapper.get('[data-test="save-button"]')
-  expect(elem.exists()).toBeTruthy()
+  test('Mount', () => {
+    expect(wrapper).toBeTruthy()
+  })
+  
+  test('Should have a headline', () => {
+    const elem = wrapper.get('[data-test="headline"]')
+    expect(elem.exists()).toBeTruthy()
+  })
+  
+  test('Should have an input name', () => {
+    const elem = wrapper.get('[data-test="input-name"]')
+    expect(elem.exists()).toBeTruthy()
+  })
+  
+  test('Should have an input longitude', () => {
+    const elem = wrapper.get('[data-test="input-longitude"]')
+    expect(elem.exists()).toBeTruthy()
+  })
+  
+  test('Should have an input latitude', () => {
+    const elem = wrapper.get('[data-test="input-latitude"]')
+    expect(elem.exists()).toBeTruthy()
+  })
+  
+  test('Should have a regenrate key button', () => {
+    const elem = wrapper.get('[data-test="revoke-btn"]')
+    expect(elem.exists()).toBeTruthy()
+  })
+  
+  test('Should have a download button', () => {
+    const elem = wrapper.get('[data-test="download-btn"]')
+    expect(elem.exists()).toBeTruthy()
+  })
+  
+  test('Should have a save button', () => {
+    const elem = wrapper.get('[data-test="save-button"]')
+    expect(elem.exists()).toBeTruthy()
+  })
 })

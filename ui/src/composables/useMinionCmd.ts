4503a82149c17d92010a4eaa5575108d0279f384
mikewrosey <MRose@nanthealth.com>
2023-06-21 14:42:33 -0400

    trim ports, use .join for legibility

--- ui/src/composables/useMinionCmd.ts
+++ ui/src/composables/useMinionCmd.ts
@@ -15,9 +15,23 @@ const useMinionCmd = () => {
       : 'minion.onms-fb-prod.production.prod.dataservice.opennms.com'
   )
 
-  const minionDockerCmd = computed(
-    () =>
-      `docker run --rm -p 8181:8181 -p 8101:8101 -p 1162:1162/udp -p 8877:8877/udp -p 4729:4729/udp -p 9999:9999/udp -p 162:162/udp -e TZ='America/New_York' -e USE_KUBERNETES="false" -e MINION_GATEWAY_HOST="${url.value}" -e MINION_GATEWAY_PORT=443 -e MINION_GATEWAY_TLS="true" -e GRPC_CLIENT_KEYSTORE='/opt/karaf/minion.p12' -e GRPC_CLIENT_KEYSTORE_PASSWORD='${password.value}'  -e MINION_ID='${minionId.value}'  --mount type=bind,source="/pathToFile/${minionId.value}-certificate.p12",target="/opt/karaf/minion.p12",readonly opennms/lokahi-minion:latest`
+  const minionDockerCmd = computed<string>(() =>
+    [
+      `docker run --rm`,
+      `-p 162:162/udp`,
+      `-p 1162:1162/udp`,
+      `-p 9999:9999/udp`,
+      `-e TZ='America/New_York'`,
+      `-e USE_KUBERNETES="false"`,
+      `-e MINION_GATEWAY_HOST="${url.value}"`,
+      `-e MINION_GATEWAY_PORT=443`,
+      `-e MINION_GATEWAY_TLS="true"`,
+      `-e GRPC_CLIENT_KEYSTORE='/opt/karaf/minion.p12'`,
+      `-e GRPC_CLIENT_KEYSTORE_PASSWORD='${password.value}'`,
+      `-e MINION_ID='${minionId.value}'`,
+      `--mount type=bind,source="/pathToFile/${minionId.value}-certificate.p12",target="/opt/karaf/minion.p12",readonly`,
+      `opennms/lokahi-minion:latest`
+    ].join(' ')
   )
 
   return { minionDockerCmd, setPassword, setMinionId, clearMinionCmdVals }

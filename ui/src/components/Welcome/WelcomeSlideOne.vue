be7308f8558786fe42a4381b8dc4cb4268747bb5
scthompsonopen <86311553+scthompsonopen@users.noreply.github.com>
2023-07-17 10:48:40 -0300

    Update ui/src/components/Welcome/WelcomeSlideOne.vue
    Co-authored-by: Bonrob2 <59453630+Bonrob2@users.noreply.github.com>

--- ui/src/components/Welcome/WelcomeSlideOne.vue
+++ ui/src/components/Welcome/WelcomeSlideOne.vue
@@ -4,7 +4,7 @@
         <div class="welcome-text">
             <h1 data-test="welcome-slide-one-title">Welcome to <br />OpenNMS&nbsp;Cloud</h1>
             <p class="margin-bottom">To start monitoring, you need to install our Minion on your system. This lightweight,
-                secure collector monitors and communicates with the devices on your network and shares collected data with
+                secure collector monitors and communicates with the devices on your network, shares collected data,
                 OpenNMS Cloud. The Minion connects your network with OpenNMS&nbsp;Cloud. </p>
             <p class="margin-bottom">We’ll walk you through this setup, which takes only a few&nbsp;minutes.
             </p>

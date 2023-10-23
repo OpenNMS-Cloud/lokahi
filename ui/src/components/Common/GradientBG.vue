46d2ff6d2677515d6f93ca791ba59cc9bb11dea4
mikewrosey <MRose@nanthealth.com>
2023-10-23 12:07:14 -0400

    add new welcome screen gradient

--- ui/src/components/Common/GradientBG.vue
+++ ui/src/components/Common/GradientBG.vue
@@ -1,37 +1,158 @@
 <template>
-  <svg :width="windowWidth" :height="windowHeight" :viewBox="`0 0 ${windowWidth} ${windowHeight}`" fill="none"
-    xmlns="http://www.w3.org/2000/svg" data-test="gradient-bg">
-    <g clip-path="url(#gradient-bg-clipping-path)">
-      <g opacity="0.7" style="filter:url(#blur);">
-        <path d="M507.711 1026.44L-14 1259H1511V634H876.921C716.395 634 563.895 895.628 507.711 1026.44Z"
-          fill="#FF555E" />
-        <ellipse opacity="0.5" cx="1068" cy="869" rx="424" ry="423" fill="#14D1DF" />
-        <ellipse opacity="0.5" cx="1328" cy="545.5" rx="146" ry="213.5" fill="#131736" />
-      </g>
-      <g opacity="0.7" style="filter:url(#blur2);mix-blend-mode:overlay;">
-        <ellipse cx="330.992" cy="345.595" rx="330.992" ry="345.595"
-          transform="matrix(0.9957 -0.0926363 0.0350879 0.999384 438.711 568.249)" fill="#0081AD" />
-        <ellipse cx="399.125" cy="252.537" rx="399.125" ry="252.537"
-          transform="matrix(0.999999 0.00132984 0.0704685 0.997514 806.184 806)" fill="#7289FF" />
-        <ellipse cx="1485.65" cy="1077.87" rx="154.432" ry="308.865" fill="#14D1DF" />
-        <ellipse cx="480.774" cy="987" rx="608.774" ry="218" fill="#273180" />
-      </g>
+  <svg
+    width="1512"
+    height="1177"
+    viewBox="0 0 1512 1177"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_3141_7369)">
+      <rect
+        width="1512"
+        height="1177"
+        fill="url(#paint0_radial_3141_7369)"
+      />
+      <rect
+        width="1512"
+        height="1177"
+        fill="url(#paint1_radial_3141_7369)"
+      />
+      <rect
+        width="1512"
+        height="1177"
+        fill="url(#paint2_radial_3141_7369)"
+      />
+      <rect
+        width="1512"
+        height="1177"
+        fill="url(#paint3_radial_3141_7369)"
+      />
+      <rect
+        width="1512"
+        height="1177"
+        fill="url(#paint4_radial_3141_7369)"
+      />
+      <rect
+        width="1512"
+        height="1177"
+        fill="url(#paint5_radial_3141_7369)"
+      />
     </g>
     <defs>
-      <filter id="blur" x="-814" y="-468" width="3125" height="2560" filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" />
-        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" />
-        <feGaussianBlur stdDeviation="400" />
-      </filter>
-      <filter id="blur2" x="-828" y="-163.793" width="3168.08" height="2250.52" filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" />
-        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" />
-        <feGaussianBlur stdDeviation="350" />
-      </filter>
-      <clipPath id="gradient-bg-clipping-path">
-        <rect :width="windowWidth" :height="windowHeight" fill="white" />
+      <radialGradient
+        id="paint0_radial_3141_7369"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(1091.5 5.12662e-05) rotate(90.1756) scale(652.503 838.22)"
+      >
+        <stop
+          stop-color="#5CFF98"
+          stop-opacity="0.15"
+        />
+        <stop
+          offset="1"
+          stop-color="#5CFF98"
+          stop-opacity="0"
+        />
+      </radialGradient>
+      <radialGradient
+        id="paint1_radial_3141_7369"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(470.5 9.96868e-06) rotate(54.9561) scale(1085.85 1394.91)"
+      >
+        <stop
+          stop-color="#007EA8"
+          stop-opacity="0.15"
+        />
+        <stop
+          offset="1"
+          stop-color="#007EA8"
+          stop-opacity="0"
+        />
+      </radialGradient>
+      <radialGradient
+        id="paint2_radial_3141_7369"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(756 2.94513e-05) rotate(90) scale(1162 1492.73)"
+      >
+        <stop
+          stop-color="#007EA8"
+          stop-opacity="0.2"
+        />
+        <stop
+          offset="1"
+          stop-color="#007EA8"
+          stop-opacity="0"
+        />
+      </radialGradient>
+      <radialGradient
+        id="paint3_radial_3141_7369"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="rotate(90) scale(930 1194.7)"
+      >
+        <stop
+          stop-color="#000524"
+          stop-opacity="0.2"
+        />
+        <stop
+          offset="1"
+          stop-color="#000524"
+          stop-opacity="0"
+        />
+      </radialGradient>
+      <radialGradient
+        id="paint4_radial_3141_7369"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="rotate(90) scale(1014.5 1109.77)"
+      >
+        <stop
+          stop-color="#FF525D"
+          stop-opacity="0.15"
+        />
+        <stop
+          offset="1"
+          stop-color="#FF4D58"
+          stop-opacity="0"
+        />
+      </radialGradient>
+      <radialGradient
+        id="paint5_radial_3141_7369"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(486.5 3.26683e-05) rotate(90) scale(568.5 730.307)"
+      >
+        <stop
+          stop-color="#708AFF"
+          stop-opacity="0.2"
+        />
+        <stop
+          offset="1"
+          stop-color="#708AFF"
+          stop-opacity="0"
+        />
+      </radialGradient>
+      <clipPath id="clip0_3141_7369">
+        <rect
+          width="1512"
+          height="1177"
+          fill="white"
+        />
       </clipPath>
     </defs>
   </svg>

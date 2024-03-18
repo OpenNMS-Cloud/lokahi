eb389d086e4588b32cdaefb81aee59a016acfefd
MFaizan <muhammad.faizan@dataqhealth.com>
2024-03-18 15:18:04 +0500

    FE: Unit tests - getAlertsByNode

--- ui/tests/store/Views/nodeStatusStore.test.ts
+++ ui/tests/store/Views/nodeStatusStore.test.ts
@@ -3,15 +3,29 @@ import { setActiveClient, useClient } from 'villus'
 import { createTestingPinia } from '@pinia/testing'
 import { useNodeStatusQueries } from '@/store/Queries/nodeStatusQueries'
 import { useNodeMutations } from '@/store/Mutations/nodeMutations'
+import { AlertsFilters, Pagination } from '@/types/alerts'
+import { TimeRange } from '@/types/graphql'
 
 describe('Node Status Store', () => {
+  let store: any
   beforeEach(() => {
     createTestingPinia({ stubActions: false })
     setActiveClient(useClient({ url: 'http://test/graphql' })) // Create and set a client
+
+    store = useNodeStatusStore()
+    store.fetchAlertsByNodeData.value = {
+      totalAlerts: 20, // Set some initial total alerts for testing pagination
+      alerts: [] // You can add some dummy data for testing if needed
+    }
   })
 
   afterEach(() => {
     vi.restoreAllMocks()
+    store.alertsPagination.value = {
+      page: 0,
+      pageSize: 10,
+      total: 0
+    }
   })
 
   // Skipping this test until flows are fully enabled
@@ -70,5 +84,51 @@ describe('Node Status Store', () => {
         nodeAlias: mockNodeAlias
       }
     })
+  })
+
+
+  it('calls getAlertsByNodeQuery with correct parameters and updates fetchAlertsByNodeData and alertsPagination', async () => {
+    const queries = useNodeStatusQueries()
+    const sortFilter: AlertsFilters = {
+      timeRange: TimeRange.All,
+      nodeLabel: '',
+      severities: [],
+      sortAscending: true,
+      sortBy: 'id',
+      nodeId: 1
+    }
+
+    const paginationFilter: Pagination = {
+      page: 0, // FE pagination component has base 1 (first page)
+      pageSize: 10,
+      total: 0
+    }
+
+    store.alertsFilter = sortFilter
+    store.alertsPagination = paginationFilter
+    await store.getAlertsByNode()
+    vi.spyOn(queries, 'getAlertsByNodeQuery')
+    queries.getAlertsByNodeQuery(sortFilter, paginationFilter)
+    expect(queries.getAlertsByNodeQuery).toHaveBeenCalledWith(sortFilter, paginationFilter)
+    expect(store.fetchAlertsByNodeData.totalAlerts).toBe(0)
+    expect(store.alertsPagination.total).toBe(0)
+  })
+
+
+  it('calls getAlertsByNode when setAlertsByNodePage is called and updates alertsPagination', async () => {
+    const getAlertsByNodeMock = vi.spyOn(store, 'getAlertsByNode')
+    await store.setAlertsByNodePage(2)
+    await store.getAlertsByNode()
+    expect(store.alertsPagination.page).toBe(2)
+    expect(getAlertsByNodeMock).toHaveBeenCalled()
+  })
+
+
+  it('calls getAlertsByNode when setAlertsByNodePageSize is called and updates alertsPagination', async () => {
+    const getAlertsByNodeMock = vi.spyOn(store, 'getAlertsByNode')
+    await store.setAlertsByNodePageSize(20)
+    await store.getAlertsByNode()
+    expect(store.alertsPagination.pageSize).toBe(20)
+    expect(getAlertsByNodeMock).toHaveBeenCalled()
   })
 })

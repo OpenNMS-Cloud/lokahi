fc67eafc868a0698192c83860d1c228e63e8f5a4
Shahbaz-Dataq-FE <shahbaz.masood@dataqhealth.com>
2024-03-28 20:19:42 +0500

    Unit tests for monitoringPoliciesStore 	monitoringPolicies.countAlertsForRule 	monitoringPolicies.removeRule

--- ui/tests/store/Views/monitoringPoliciesStore.test.ts
+++ ui/tests/store/Views/monitoringPoliciesStore.test.ts
@@ -1,7 +1,9 @@
-import { EventType, PolicyRule } from '@/types/graphql'
+// import { useMonitoringPoliciesStore } from '@/store/Views/monitoringPoliciesStore'
+import { PolicyRule } from '@/types/graphql'
 import { Policy, ThresholdCondition } from '@/types/policies'
 import { createTestingPinia } from '@pinia/testing'
 import { mockMonitoringPolicy, monitoringPolicyFixture } from 'tests/fixture/monitoringPolicy'
+
 import { setActiveClient, useClient } from 'villus'
 
 describe('Node Status Store', () => {
@@ -129,19 +131,19 @@ describe('Node Status Store', () => {
     store.selectedPolicy = {...mockMonitoringPolicy, isDefault: false}
     store.selectedRule = mockMonitoringPolicy && mockMonitoringPolicy.rules ? mockMonitoringPolicy?.rules[0] : {}
 
-    vi.spyOn(store, 'updateCondition')
+    vi.spyOn(store, 'deleteCondition')
     store.deleteCondition('2')
     expect(store.deleteCondition).toHaveBeenCalledWith('2')
   })
 
-  it('monitoringPolicies.deleteCondition complete successfully', async () => {
+  it('monitoringPolicies.validateRule complete successfully', async () => {
     const { useMonitoringPoliciesStore } = await import('@/store/Views/monitoringPoliciesStore')
     const store = useMonitoringPoliciesStore()
 
     store.selectedPolicy = {...mockMonitoringPolicy, isDefault: false}
     store.selectedRule = mockMonitoringPolicy && mockMonitoringPolicy.rules ? mockMonitoringPolicy?.rules[0] : {}
 
-    vi.spyOn(store, 'updateCondition')
+    vi.spyOn(store, 'validateRule')
     store.validateRule(store.selectedRule)
     expect(store.validateRule).toHaveBeenCalledWith(store.selectedRule)
     expect(store.validateRule).toBeTruthy()
@@ -186,5 +188,58 @@ describe('Node Status Store', () => {
     })
   })
 
+  it.skip('monitoringPolicies.removeRule complete successfully', async () => {
+    // const mutations - useMonitoringPoliciesMutations
+    vi.doMock('@/store/Mutations/monitoringPoliciesMutations', async () => {
+      const actual = await vi.importActual('@/store/Mutations/monitoringPoliciesMutations')
+      return {
+        actual,
+        // deleteRule: {
+        useMutation: vi.fn(() => ({
+          execute: vi.fn(),
+          isFetching: vi.fn(),
+          error: vi.fn()
+        }))
+        // }
+      }
+    })
+    const { useMonitoringPoliciesStore } = await import('@/store/Views/monitoringPoliciesStore')
+    const store = useMonitoringPoliciesStore()
 
+    store.selectedPolicy = {...mockMonitoringPolicy, isDefault: false}
+    store.selectedRule = mockMonitoringPolicy && mockMonitoringPolicy.rules ? mockMonitoringPolicy?.rules[0] : {}
+
+    vi.spyOn(store, 'removeRule')
+    await store.removeRule()
+    expect(store.removeRule).toBeCalled()
+  })
+
+  it.skip('monitoringPolicies.countAlertsForRule complete successfully', async () => {
+    const mockData = {
+      value: {
+        countAlertByRuleId: 1
+      }
+    }
+    vi.doMock('villus', () => ({
+      useQuery: vi.fn().mockImplementation(() => ({
+        data: mockData,
+        execute: vi.fn().mockResolvedValue(mockData),
+        isFetching: {
+          value: false
+        }
+      }))
+    }))
+    const { useMonitoringPoliciesStore } = await import('@/store/Views/monitoringPoliciesStore')
+    const store = useMonitoringPoliciesStore()
+
+    store.selectedPolicy = {...mockMonitoringPolicy, isDefault: false}
+    store.selectedRule = mockMonitoringPolicy && mockMonitoringPolicy.rules ? mockMonitoringPolicy?.rules[0] : {}
+
+    vi.spyOn(store, 'countAlertsForRule')
+    await store.countAlertsForRule()
+
+    // console.log(".......................", store.numOfAlertsForRule);
+
+    expect(store.numOfAlertsForRule >= 0).toBe(true)
+  })
 })
